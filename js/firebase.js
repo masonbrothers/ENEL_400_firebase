dc8f7be1761f6b750ac62f39ef6bed6249f34da5
Mason Brothers <brothers.mason@gmail.com>
2016-10-16 12:22:32 -0600

    Moved files

---
+++ js/firebase.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD-xCFV4yVZO5uJg66pRRR_xtvCqOrcFUs",
+    authDomain: "aquaponics-monitoring.firebaseapp.com",
+    databaseURL: "https://aquaponics-monitoring.firebaseio.com",
+    storageBucket: "aquaponics-monitoring.appspot.com",
+    messagingSenderId: "354454143922"
+  };
+  firebase.initializeApp(config);
